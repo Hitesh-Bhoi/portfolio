07af4f9880e51bd8b8944af4d483b5c9573f3c16
bhoi hitesh <bhoihitesh1492@gmail.com>
2026-09-06 10:44:18 +0530

    feat: added scripts to automate test cases with codegen

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('navigation').getByRole('link', { name: 'About' }).click();
+  await page.locator('#hero').click();
+  await page.locator('#about').click();
+  await page.locator('#skills').click();
+  await page.locator('#projects').click();
 });
